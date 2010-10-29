bb832832004fbdc07707c71a2a52aee968b3fb02
Michał Górny <mgorny@gentoo.org>
2010-10-29 23:25:40 +0200

    Reimplement SyncedUnmaskFile class.
    Now, instead of dirty comparing and sorting we're using a real live
    class to keep the unmask file in sync. It operates directly on the mask
    entries, marking those supposedly used within the unmask file.

--- lib/diffmask/syncedunmaskfile.py
+++ lib/diffmask/syncedunmaskfile.py
@@ -2,78 +2,75 @@
 #	vim:fileencoding=utf-8
 # (C) 2010 Michał Górny, distributed under the terms of 3-clause BSD license
 
+from copy import copy
+
 from diffmask.maskfile import MaskFile
 from diffmask.unmaskfile import UnmaskFile
 from diffmask.util import DiffmaskList
 
 class SyncedUnmaskFile(UnmaskFile):
-	""" A class representing the package.unmask file being up-to-date
-		with the package.mask files. In other words, all mask entries
-		in the SyncedUnmaskFile correspond to package.mask entries
-		and are ordered like them. """
+	""" The package.unmask file with forced sync to package.mask files.
+		In other words, this class is going to accept only package
+		entries which match package.mask, and will output them in
+		package.mask order. """
 	class SyncedUnmaskRepo(MaskFile.MaskRepo):
-		""" A class representing a single repository in the
-			package.unmask file. """
-		def __init__(self, maskr, unmaskr, noner):
-			""" Instantiate the new SyncedUnmaskRepo. Try to match
-				entries from the original package.unmask file (being
-				passed as `unmaskr' and `noner' for the entries
-				associated with the repo and those being unassociated
-				respectively) with the package.mask repository `maskr'.
+		""" A single repository within SyncedUnmaskFile. """
+		def __init__(self, origrepo):
+			""" Instantiate the SyncedUnmaskRepo, copying the entries
+				from `origrepo' (MaskRepo instance). """
+			self.name = origrepo.name
+			for e in origrepo:
+				ce = copy(e)
+				ce._enabled = False
+				DiffmaskList.append(self, ce)
+
+		def append(self, entry):
+			""" `Append' a block to the repository, checking if it does
+				match the package.mask file. Technically, this function
+				simply enables appropriate entries copied in __init__().
 				"""
-			MaskFile.MaskRepo.__init__(self, maskr.name)
-			tblocks = []
+			if not isinstance(entry, self.MaskBlock):
+				entry = self.MaskBlock(entry)
 
-			for b in unmaskr + noner:
-				# first try to get exact match
-				cb = maskr.find(b)
-				if cb is not None:
-					tblocks.append(cb)
-				else:
-					# then try partial matches
-					# (both comment & atoms as they might come
-					# from a set of different masks)
-					for cb in maskr:
-						if cb.comment == b.comment:
-							tblocks.append(cb)
-							continue
-						for a in b:
-							if a in cb:
-								tblocks.append(cb)
-								break
+			e = self.find(entry)
+			if e is not None:
+				e._enabled = True
+			else:
+				# Now try to get a partial match as atoms floating
+				# around might have been appended to the block.
+				for e in self:
+					if e.comment == entry.comment:
+						e._enabled = True
+					for a in entry:
+						if a in e:
+							e._enabled = True
 
-			# sort & uniq, now we have to get exact matches
-			for cb in maskr:
-				if cb in tblocks:
-					self.append(cb)
-					tblocks.remove(cb)
-			if tblocks:
-				raise AssertionError('At least a single match failed in the sorting loop')
+		def extend(self, entries):
+			for e in entries:
+				self.append(e)
+
+		def toString(self):
+			out = [x.toString() for x in self if x._enabled]
+			if out and self.name:
+				out.insert(0, '## *%s*\n\n' % self.name)
+			return ''.join(out)
+
+	def append(self, repo):
+		if not isinstance(repo, self.SyncedUnmaskRepo):
+			repo = self.SyncedUnmaskRepo(repo)
+		DiffmaskList.append(self, repo)
 
 	def __init__(self, mask, unmask):
-		""" Instantiate the new SyncedUnmaskFile. Try to match entries
-			from the original package.unmask file (being passed as
-			`unmask') and the package.mask files (being passed as
-			`mask'). Unmatched entries will be dropped. """
+		""" Instantiate the SyncedUnmaskFile using a package.mask file
+			instance `mask' and UnmaskFile instance `unmask'. """
 		DiffmaskList.__init__(self)
 		self.path = unmask.path
 
-		# find the repo containing unmatched entries
-		# (in fact it should be always the first one but better safe than sorry)
-		try:
-			nonerepo = unmask[None]
-		except KeyError:
-			nonerepo = []
-
-		for mr in mask:
-			if mr.name is not None:
-				try:
-					r = unmask[mr.name]
-				except KeyError: # repo only in mask file
-					r = []
-
-				outr = self.SyncedUnmaskRepo(mr, r, nonerepo)
-				if outr:
-					self.append(outr)
-
-		# repos which are only in unmask file are dropped implicitly
+		for r in mask:
+			self.append(r)
+		for ur in unmask:
+			if ur.name is None:
+				for r in self:
+					r.extend(ur)
+			elif ur.name in self:
+				self[ur.name].extend(ur)
